8bab67583d048b9af495feca0e8b6603dd4faa0e
nahiarasanchez <96417033+nahiarasanchez@users.noreply.github.com>
2022-05-09 09:16:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert ("Bienvenido al examen final");
+var nombre=prompt("Por favor, introduzca su nombre");
+alert ("Hola, " + nombre + " bienvenido al examen");
+var vocal= ("a", "e", "i", "o", "u");
+if (nombre[0]== vocal);
+document.write ("<p> La primera letra de tu nombre es una vocal</p>");
+
+for (let i = nombre[0]; i < nombre.length; i++) {
+  text += nombre[i] + "<br>";
+if (nombre[i]==vocal) {
+  document.write (nombre[i] + " es una vocal")};
